d953a0948022426b175840894d27e1ea627a715c
denise <denise.j.ferreira@gmail.com>
2014-04-06 00:03:34 -0300

    filtrando dados do ibge

--- shell/filtro.sh
+++ shell/filtro.sh
@@ -1,1 +1,4 @@
-egrep '^33' PMEnova.012014.txt 
+#eval echo \$curso$num
+curso01='Regular do ensino fundamental ou do 1° grau'
+
+egrep '^33' ../ibge/PMEnova.012014.txt | cut -c104,105-112,151-152 --output-delimiter=, | sed s/^1/HOMEM/ | sed s/^2/MULHER/
