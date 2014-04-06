95b3e740338e4d41b81d169f9a2eaafec49eaf1a
denise <denise.j.ferreira@gmail.com>
2014-04-06 12:28:57 -0300

    leitura de entrada do nome do arquivo

--- shell/filtro.sh
+++ shell/filtro.sh
@@ -1,3 +1,10 @@
+echo "Digite o caminho do arquivo: "
+read path
+
+[ -z $path ] && echo "Erro: Passe o caminho do arquivo" && exit 1 
+[ ! -f $path ] && echo "Erro: Passe um caminho válido" && exit 1
+
+echo "Criando Arquivo dados.csv"
 
 curso01='Regular do ensino fundamental ou do 1° grau'
 curso02='Regular do ensino médio ou do 2º grau'
@@ -13,13 +20,13 @@ curso09='Mestrado ou doutorado'
 # separa colunas sobre sexo, data nascimento e curso respectivamente
 # primeiro caracter, transforma 1 em homem e 2 em mulher
 
-egrep '^33' ../ibge/PMEnova.012014.txt | cut -c104,105-112,151-152 --output-delimiter=, | sed s/^1/HOMEM/ | sed s/^2/MULHER/ | while read line 
+egrep '^33' $path  | cut -c104,105-112,151-152 --output-delimiter=, | sed s/^1/HOMEM/ | sed s/^2/MULHER/ | while read line 
 do
 	curso=$(echo $line| cut -d, -f3| sed -E "s/([0-9]{2})$/\$curso\1/")
 	newLine=$(echo $line | sed -E "s/[0-9]{2}$/$(eval echo $curso)/")
 	echo $newLine >> dados.csv
+
+	# echo $newLine
 done 
 
 
-
-#echo $filter
